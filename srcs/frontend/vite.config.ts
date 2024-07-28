75fd8ffb48176e3a47b52be35bd3f90b3b60470b
manettifabrizio <fmanetti@student.42.fr>
2024-07-28 13:58:35 +0000

    docker compose and vite.config for prod

--- srcs/frontend/vite.config.ts
+++ srcs/frontend/vite.config.ts
@@ -11,11 +11,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080, // cf. Docker/Nginx configuration file.
-    hmr: {
-      path: '/hmr', // Change default to avoid conflict with server sockets.
-      port: 443,
-      clientPort: 4443,
-    },
   },
   resolve: {
     alias: {
